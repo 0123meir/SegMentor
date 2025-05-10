11a4a36de4dc86ce087bfe0d0e43ff8a3d8b10d1
Liraz <liraz581cohen@gmail.com>
2025-05-10 16:42:09 +0300

    added client side login and register pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AIAssistant } from "@/components/AIAssistant";
 import { Collections } from "@/components/Collections";
 import LecturePlayer from "@/components/LecturePlayer";
+import LoginPage from '@/components/LoginPage.tsx';
+import RegisterPage from '@/components/RegisterPage.tsx';
+import ProtectedRoute from '@/components/ProtectedRoute.tsx';
 
 const App = () => {
   return (
-    <div className="flex h-screen">
-      <div className="flex flex-col flex-grow">
-       <LecturePlayer/>
-
-        <AIAssistant/>
-      </div>
-
-      <Collections/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <div className="flex h-screen">
+                <div className="flex flex-col flex-grow">
+                  <LecturePlayer />
+                  <AIAssistant />
+                </div>
+                <Collections />
+              </div>
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
 };
 
