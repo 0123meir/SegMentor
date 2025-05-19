0b1529b9e427665e289d7ec88e93ebc75cfc38c8
Asaf <asaflumer@gmail.com>
2025-05-19 21:30:34 +0300

    Removed unused comments

--- frontend/src/components/LecturePlayer.tsx
+++ frontend/src/components/LecturePlayer.tsx
@@ -1,37 +1,15 @@
-// import { useFileUploader } from '@/hooks/useFileUploader';
 import { useEffect, useState } from 'react';
 
-// import FileDropZone from './FileDropZone';
-// import UploadSnackbar from './UploadSnackbar';
 import VideoPlayer from './VideoPlayer';
 import { useCoursesStore } from '@/state/CoursesStore';
 import { useSegmentsStore } from '@/state/SegmentsStore';
 import useAuthStore from '@/stores/AuthStore';
 
 const LecturePlayer = () => {
-  // const [videoFile, setVideoFile] = useState<File | null>(null);
   const [videoUrl, setVideoUrl] = useState<string | undefined>(undefined);
   const {activeLectureId} = useCoursesStore()
   const {setSegments} = useSegmentsStore()
   const {token} = useAuthStore();
-
-  // const { uploadFile, uploadState } = useFileUploader();
-
-  // const handleFileUpload = (acceptedFiles: File[]) => {
-  //   const file = acceptedFiles[0];
-
-  //   if (file.type.startsWith('video/') && !videoFile) {
-  //     setVideoFile(file);
-  //     setVideoUrl(URL.createObjectURL(file));
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   if (videoFile) {
-  //     console.log('start upload of ', videoFile);
-  //     uploadFile(videoFile);
-  //   }
-  // }, [videoFile]);
 
   useEffect(() => {
     const onActiveLectureChange = async () => {
@@ -46,20 +24,9 @@ const LecturePlayer = () => {
 
   return (
     <div className="flex flex-grow m-2 gap-1" style={{ height: '80rem' }}>
-      {/* {!videoFile && (
-        <FileDropZone
-          dropZoneOptions={{
-            accept: { 'video/mp4': ['.mp4'] },
-            onDrop: handleFileUpload,
-          }}
-        />
-      )} */}
-
       {videoUrl ? (
         <VideoPlayer url={videoUrl} />
       ): <>No Lecture Selected!</>}
-
-      {/* <UploadSnackbar uploadState={uploadState} /> */}
     </div>
   );
 };
