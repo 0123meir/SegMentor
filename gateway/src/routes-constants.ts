18e21ef8bc83fd2de093b924b410a7552c5294d2
yoni1920 <yoni.81416@gmail.com>
2025-05-26 22:22:51 +0300

    docker compose fixes

--- gateway/src/routes-constants.ts
+++ gateway/src/routes-constants.ts
@@ -1,4 +1,5 @@
 export const PERMISSIONS_URL =
   process.env.PERMISSION_SERVICE_URL || 'http://localhost:3001';
+
 export const COURSES_URL =
   process.env.COURSES_SERVICE_URL || 'http://localhost:3002';
