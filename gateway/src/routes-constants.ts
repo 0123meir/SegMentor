1e633577781d7ffc9c47fe86d6f014d53a86fb2b
Asaf <asaflumer@gmail.com>
2025-05-28 21:25:15 +0300

    Added transcript service to API GW

--- gateway/src/routes-constants.ts
+++ gateway/src/routes-constants.ts
@@ -3,3 +3,6 @@ export const PERMISSIONS_URL =
 
 export const COURSES_URL =
   process.env.COURSES_SERVICE_URL || 'http://localhost:3002';
+
+export const TRANSCRIPT_URL =
+  process.env.TRANSCRIPT_SERVICE_URL || 'http://localhost:3006';
