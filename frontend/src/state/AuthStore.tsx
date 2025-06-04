a69fd9933ce8f8a075665f5dd9024c08e744da6c
Meir <0123meir@gmail.com>
2025-06-04 18:30:59 +0300

    WIP on Develop

--- frontend/src/state/AuthStore.tsx
+++ frontend/src/state/AuthStore.tsx
@@ -10,13 +10,17 @@ interface User {
   username: string;
   role: UserRoles;
 }
-
+interface UserDTO {
+  id: string;
+  username: string;
+  role: string;
+}
 interface AuthState {
   token: string;
   user: User | null;
   setToken: (token: string) => void;
   clearToken: () => void;
-  setUser: (user: User) => void;
+  setUser: (user: UserDTO) => void;
   clearState: () => void;
 }
 
@@ -25,7 +29,7 @@ const useAuthStore = create<AuthState>((set) => ({
   user: null,
   setToken: (token) => set({ token }),
   clearToken: () => set({ token: '' }),
-  setUser: (user) => set({ user }),
+  setUser: (user) => set({ user: { ...user, role: user.role as UserRoles } }),
   clearState: () => set({ token: '', user: null })
 }));
 
